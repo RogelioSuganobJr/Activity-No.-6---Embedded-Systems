5938ca8135245e22db2a343f1704a55ed89a13b2
Rogelio Suganob Jr <92284274+RogelioSuganobJr@users.noreply.github.com>
2022-11-04 18:28:32 +0800

    Files Updated

--- round_robin.c
+++ round_robin.c
@@ -4,9 +4,10 @@
 
 int bt[] = {2, 8, 1, 3, 10, 5}; // Burst time inputs
 int p[] = {8, 7, 6, 5, 4, 3}; // Processes pin inputs
-int temp[] = {};
-int size = 6, time_quantum = 2, count = 20;
-float AWT, wt;
+int temp[] = {0, 0, 0, 0, 0, 0};
+int size = 6, time_quantum = 2, count = 20, t = 0;
+float AWT, ft;
+// Arrives at time 0
 
 void setup() {
   // LEDs Pin configuration
@@ -22,22 +23,35 @@ void setup() {
 void loop() {
   while (count != 0) {
     for (int i = 0; i < size; i++) {
-      if (bt[i] < time_quantum && bt[i] != 0) {
-        Arrive(p[i], time_quantum - 1);
+      if (t < size) {
+        temp[t] = bt[i]; // a copy of burst time
+      }
+      if (bt[i] <= time_quantum && bt[i] != 0) { // end of single task
+        if (bt[i] < time_quantum){ // if remaining burst time is less than time quantum
+          Arrive(p[i], time_quantum - bt[i]);
+          ft = ft + 1;
+          Wt(ft - temp[i]); // Finish time - burst time
+        }
+        if (bt[i] == time_quantum) { // if remaining burst time is equal to time quantum
+          Arrive(p[i], time_quantum);
+          ft = ft + 2;
+          Wt(ft - temp[i]); // Finish time - burst time
+        }
         bt[i] = 0;
-      } else if (bt[i] >= time_quantum) {
+        
+      } else if (bt[i] > time_quantum) { // if remaining burst time is greater than time quantum
         bt[i] = bt[i] - time_quantum;
       	Arrive(p[i], time_quantum);
+        ft = ft + 2; 
       } else {
-        count--;
+        count--; // terminator
       }
+         t++;
     }
   }
   
-  /*
   Serial.println("Average Waiting Time : ");
   Serial.println(AWT/size); // Prints Average Waiting Time
-  */
   Serial.println("End of process.");
   delay(10000);
   
@@ -49,12 +63,9 @@ void Arrive(int task, int timeQuantum) {
   digitalWrite(task, LOW);
 }
 
-/*
-void Wt(int waitingTime) {
-  wt = wt + waitingTime; 
-  AWT = AWT + wt;
+void Wt(int waitingTime) { // Summation of waiting time
+  AWT = AWT + waitingTime;
 }
-*/
 
 
         
